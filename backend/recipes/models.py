4cdf8a8ca852428e86c22a53713de1e113a47492
Влад Буганов <Vladislav-buganov@yandex.ru>
2023-09-16 10:43:00 +0300

    Исправления после ревью

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,9 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from users.models import User
-
-
 LONG=200
+
 
 class Tag(models.Model):
     """
